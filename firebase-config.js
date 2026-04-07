d50bcd6352ab239e54ff804c57f8c53ec94d50ee
salah405 <hotspotcoffe818@gmail.com>
2026-04-07 14:52:34 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Config لمشروع amboba-a54b1
+window.firebaseConfig = {
+  apiKey: "AIzaSyCIVzmvQYzSAC_i8wWLvDr5j2BsgNQIXjU",
+  authDomain: "amboba-a54b1.firebaseapp.com",
+  projectId: "amboba-a54b1",
+  storageBucket: "amboba-a54b1.firebasestorage.app",
+  messagingSenderId: "218154851411",
+  appId: "1:218154851411:web:6b57d5af156dbd3556baa6",
+  measurementId: "G-WPMTFNM11B"
+};
